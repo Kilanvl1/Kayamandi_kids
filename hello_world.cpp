1a52fcc4acd32739a2c4684d831060f2a45535e4
Kilanvl1 <90692424+Kilanvl1@users.noreply.github.com>
2023-09-29 12:09:30 +0200

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+//https://code.visualstudio.com/docs/cpp/config-mingw
+//https://github.com/msys2/msys2-installer/releases/download/2023-05-26/msys2-x86_64-20230526.exe
+//C:\msys64
+//pacman -S --needed base-devel mingw-w64-ucrt-x86_64-toolchain
+int main() {
+    std::cout<<"hello world"; 
+}
