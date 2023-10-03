371fb783848c4d3b4abc216b664c25077c12b53e
unknown <kilanvanloo@gmail.com>
2023-10-03 18:00:03 +0200

    Lesson 1

---
+++ Lesson 1/Input_output.cpp
@@ -0,0 +1,12 @@
+//Using cout and cin requires this import 
+#include <iostream>
+
+//Every program starts with this function 
+int main() {
+    //Declare a string variable 
+    std::string myName;
+    //Print to the screen  
+    std::cout<<"Enter your name: ";
+    //Store the variable 
+    std::cin>>myName;
+} 
--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,52 @@
 #include <iostream> 
-//https://code.visualstudio.com/docs/cpp/config-mingw
-//https://github.com/msys2/msys2-installer/releases/download/2023-05-26/msys2-x86_64-20230526.exe
-//C:\msys64
-//pacman -S --needed base-devel mingw-w64-ucrt-x86_64-toolchain
+#include <vector>
+
+//Starting point of every c++ program
 int main() {
-    std::cout<<"hello world"; 
-}
+    //declaring our variables
+    int age;
+    float decimalNumber; //Can store decimal numbers like 3.5
+    std::string name;
+    std::vector<int> ages;
+
+    std::cout<<"please enter your age: \n";
+    //Taking in age and printing out age 
+    
+    std::cin>>age;
+    std::cout<< "Your age is: " << age;
+    //Printing blank space/line 
+    std::cout<<"\n";
+
+    std::cout<<"please enter your name: \n";
+    //Taking in name and printing out name
+
+    std::cin>>name;
+    std::cout<<"your name is: "<< name << "\n";
+    //printing in a blank space
+    ages.push_back(age); //push_back = add
+    ages.push_back(16);
+    ages.push_back(12);
+    ages.push_back(9); 
+    //Our first conditional statement IF / ELSE / ELSE IF
+    //Always start with an IF and end with an ELSE 
+    //As many ELSE IF in between
+    if(age >= 18) {
+        std::cout<<"You are allowed to legally drink";
+    } else if(age >= 16) {
+        std::cout<< "You are between 16 and 18";
+    } else {
+        std::cout<<"You are not allowed to drink";
+    } 
+    for (int i = 0; i < ages.size(); i++)
+    {
+        std::cout<< "i has the value: " << i << "\n";
+        //std::cout<<ages.at(i) << ",";
+    }
+    
+}  
+
+//NOTES
+/*
+>> take in cin
+<< put out cout
+*/
